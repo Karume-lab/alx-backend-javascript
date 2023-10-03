c6d57008b8c8d8e24ad1f2c428c38db9289b3e68
Karume-lab <danielkarume@outlook.com>
2023-10-03 22:38:52 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,16 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  return Array.from(set)
-    .filter((word) => word.startsWith(startString))
-    .map((word) => word.slice(startString.length))
-    .join('-');
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
