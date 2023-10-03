87480239436198f63243cf3e76125bb4c267b2f0
Karume-lab <danielkarume@outlook.com>
2023-10-03 22:27:32 +0300

    More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 export default function hasValuesFromArray(set, array) {
   const unique_elems = array.filter((elem) => (!set.has(elem)));
-  return unique_elems.length === 0 ? true : false;
+  return unique_elems.length === 0;
 }
